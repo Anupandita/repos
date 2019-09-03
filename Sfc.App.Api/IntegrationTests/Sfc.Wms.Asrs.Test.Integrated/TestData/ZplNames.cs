28b7291cd766bd58ec0c590faebf7574acda2a52
ashwini.patil <ashwini_patil@shamrockfoods.com>
2019-09-03 12:41:41 +0530

    zpl

---
+++ Sfc.App.Api/IntegrationTests/Sfc.Wms.Asrs.Test.Integrated/TestData/ZplNames.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sfc.Wms.Api.Asrs.Test.Integrated.TestData
+{
+  
+    public class ZplNames
+    {
+       
+        public string CartonTotalQtyDesc { get; set; }
+        public string PalletId { get; set; }
+        public string Flags1 { get; set; }
+        public string Flags2 { get; set; }
+        public string CartonTotalQty { get; set; }
+        public string Level { get; set; }
+        public string Bay { get; set; }
+        public string Aisle { get; set; }
+        public string Area { get; set; }
+        public string ReverseCode1 { get; set; }
+        public string ReverseCode2 { get; set;}
+        public string ShipTo { get; set; }
+        public string ShipToName { get; set; }
+        public string PktSeqNbr { get; set; }
+        public string ActlDockActlDoor { get; set; }
+        public string TempZone { get; set; }
+        public string ShpmtNbr { get; set; }
+        public string CartonNbrBc { get; set; }
+        public string CaseCount { get; set; }
+        public string ShipDateTime { get; set; }
+        public string CustDept { get; set; }
+        public string Style { get; set; }
+        public string WaveNbr { get; set; }
+        public string NestVolDfltUom { get; set; }
+        public string VendorItemNbr { get; set; }
+        public string SkuDesc { get; set; }
+        public string XofY { get; set; }
+        public string Quant{ get; set; }
+    }
+}
