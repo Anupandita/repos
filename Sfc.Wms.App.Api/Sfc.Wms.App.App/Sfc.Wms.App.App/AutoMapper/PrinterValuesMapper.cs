3e63ec38a448c49ad6d0639d7fbc31bb8ff05e85
Bharani <ven_apparatus1@shamrockfoods.com>
2019-09-26 14:15:26 +0530

    Adding AutoMapper

---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.App/Sfc.Wms.App.App/AutoMapper/PrinterValuesMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Sfc.Core.OnPrem.Result;
+using Sfc.Wms.Configuration.SystemCode.Contracts.Dtos;
+
+namespace Sfc.Wms.App.App.AutoMapper
+{
+    public class PrinterValuesMapper
+    {
+        public static void CreateMaps(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<SysCodeDto, SfcPrinterSelectList>()
+                .ForMember(d => d.Id, s => s.MapFrom(p => p.CodeId))
+                .ForMember(d => d.Description, s => s.MapFrom(p => p.CodeDesc))
+                .ForMember(d => d.DisplayName, s => s.MapFrom(p => p.MiscFlag))
+                .IncludeAllDerived().ForAllOtherMembers(el => el.Ignore());
+        }
+    }
+}
