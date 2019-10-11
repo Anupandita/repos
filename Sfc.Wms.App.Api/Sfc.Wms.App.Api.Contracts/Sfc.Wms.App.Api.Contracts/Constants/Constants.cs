97ca638c414459e648037f4e647122a53bebcd35
Bharani Thungapalli <bharani.thungapalli@wmapparatus.com>
2019-10-11 05:10:04 -0700

    Merged PR 1164: Added new Constant for NodeToken
    Added new Constant for NodeToken
    
    Related work items: #119761

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api.Contracts/Sfc.Wms.App.Api.Contracts/Constants/Constants.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Contracts/Sfc.Wms.App.Api.Contracts/Constants/Constants.cs
@@ -3,6 +3,7 @@
     public static class Constants
     {
         public const string NodeToken = "NodeToken";
+        public const string Token = "Token";
         public const string Unit = "UNIT";
     }
 }
