71a715856f226008c9dfbe5a7cf5683bbb201d6d
Bharani Thungapalli <bharani.thungapalli@wmapparatus.com>
2019-10-15 07:00:42 -0700

    Merged PR 1184: Implemented Menu Route dto for UI
    Implemented Menu Route dto for UI
    
    Related work items: #134015

---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Contracts/Sfc.Wms.App.Api.Contracts/Dto/MenuRouteDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfc.Wms.App.Api.Contracts.Dto
+{
+   public class MenuRouteDto
+    {
+        public string Path { get; set; }
+    }
+}
