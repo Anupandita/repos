9837f0ec1d70a5d4441ab2187c5d222c4ef73266
ashwini.patil <ashwini_patil@shamrockfoods.com>
2019-10-17 10:39:08 +0530

    update

--- Sfc.App.Api/IntegrationTests/Sfc.Wms.Asrs.Test.Integrated/Fixtures/DataBaseFixtureForOrmt.cs
+++ Sfc.App.Api/IntegrationTests/Sfc.Wms.Asrs.Test.Integrated/Fixtures/DataBaseFixtureForOrmt.cs
@@ -5,7 +5,6 @@ using Oracle.ManagedDataAccess.Client;
 using Sfc.Wms.Interface.ParserAndTranslator.Contracts.Constants;
 using Sfc.Wms.Interface.ParserAndTranslator.Contracts.Dto;
 using Sfc.Wms.Api.Asrs.Test.Integrated.TestData;
-using Sfc.Wms.Data.Entities;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using Sfc.Wms.Foundation.Location.Contracts.Dtos;
@@ -180,13 +179,16 @@ namespace Sfc.Wms.Api.Asrs.Test.Integrated.Fixtures
         public CartonView GetValidOrderDetails(OracleConnection db, int statCode, int miscNum1)
         {
             var cartonView = new CartonView();
-            var query = $"select ch.carton_nbr,ch.wave_nbr,ch.sku_id,ch.MISC_NUM_1, ch.total_qty, ch.stat_code,ch.DEST_LOCN_ID,ph.PKT_CTRL_NBR,ph.SHIP_W_CTRL_NBR,ph.Whse,ph.CO,ph.DIV, im.spl_instr_code_1, im.spl_instr_code_5 from CARTON_HDR ch " +
+            var query = $"select ch.carton_nbr,ch.wave_nbr,ch.sku_id,pl.locn_id,ch.MISC_NUM_1, ch.total_qty, ch.stat_code,pl.actl_invn_qty,ch.DEST_LOCN_ID,ph.PKT_CTRL_NBR,ph.SHIP_W_CTRL_NBR,ph.Whse,ph.CO,ph.DIV, im.spl_instr_code_1, im.spl_instr_code_5 from CARTON_HDR ch " +
                 $"inner join PKT_HDR ph ON ph.pkt_ctrl_nbr = ch.pkt_ctrl_nbr " +
-                $"inner join Pick_Locn_dtl pl ON  pl.sku_id = ch.sku_id  " +
+                $"inner join Pick_Locn_dtl pl ON  pl.sku_id = ch.sku_id " +
                 $"inner join ITEM_MASTER im ON pl.sku_id = im.sku_id " +
-                $"inner join locn_hdr lh ON pl.locn_id = lh.locn_id " +
-                $"inner join locn_grp lg ON lg.locn_id=lh.locn_id " +
-                $"inner join sys_code sc ON sc.code_id=lg.grp_type where ch.stat_code = 12 and ch.MISC_NUM_1 = 0 and ch.MISC_INSTR_CODE_5 is null and sc.code_type='740' and code_id ='18'and lg.grp_attr = DECODE(im.temp_zone,'D','Dry','Freezer') and pick_locn_id = lh.locn_id";
+                $"inner join locn_hdr lh ON pl.locn_id = lh.locn_id" +
+                $"inner join locn_grp lg ON lg.locn_id = lh.locn_id " +
+                $"inner join sys_code sc ON sc.code_id = lg.grp_type " +
+                $"inner join pkt_hdr ph ON ph.pkt_ctrl_nbr = ch.pkt_ctrl_nbr " +
+                $"inner join alloc_invn_dtl ai ON ai.sku_id = ch.sku_id " +
+                $"where ch.stat_code = 5 and ch.misc_instr_code_5 is null and misc_num_1 = 0 and sc.code_type = '740' and code_id = '18' and lg.grp_attr = DECODE(im.temp_zone, 'D', 'Dry', 'Freezer') and pl.actl_invn_qty! = 0 and ph.pkt_stat_code < 35";
             var command = new OracleCommand(query, db);
             var Reader = command.ExecuteReader();
             if (Reader.Read())
@@ -206,6 +208,20 @@ namespace Sfc.Wms.Api.Asrs.Test.Integrated.Fixtures
             }
             return cartonView;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public List<CartonView> GetValidOrderDetailsForPrintingOfCartons(OracleConnection db)
         {
