fa2bb292b2d72d03615671602000561d483a1f7e
Bharani Thungapalli <bharani.thungapalli@wmapparatus.com>
2019-10-23 07:17:52 -0700

    Merged PR 1212: UserInfoModel Preference Issue fix.
    UserInfoModel Preference Issue fix.
    
    Related work items: #134015

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Gateways/UserGateway.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Gateways/UserGateway.cs
@@ -1,13 +1,13 @@
-﻿using RestSharp;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Threading.Tasks;
+using RestSharp;
+using Sfc.Core.OnPrem.Security.Contracts.Dtos;
 using Sfc.Wms.App.Api.Contracts.Constants;
-using Sfc.Wms.App.Api.Contracts.Result;
 using Sfc.Wms.App.Api.Contracts.Entities;
 using Sfc.Wms.App.Api.Contracts.Interfaces;
-using Sfc.Wms.App.Api.Contracts.Dto;
+using Sfc.Wms.App.Api.Contracts.Result;
 
 namespace Sfc.Wms.App.Api.Nuget.Gateways
 {
@@ -24,7 +24,77 @@ namespace Sfc.Wms.App.Api.Nuget.Gateways
             _restClient = restClient;
         }
 
-        public async Task<BaseResult<string>> SignInAsync(LoginCredentials loginCredentials )
+        public async Task<Contracts.Result.BaseResult<string>> ChangePassword(ChangePasswordModel changePasswordModel, string token)
+        {
+            return await Proxy().ExecuteAsync(async () =>
+            {
+                var request = PostChangePasswordRequest(changePasswordModel, token);
+                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
+                return _responseBuilder.GetResponseData<List<object>>(response);
+            }).ConfigureAwait(false);
+        }
+
+        public async Task<Contracts.Result.BaseResult<string>> CheckSession(string token)
+        {
+            return await Proxy().ExecuteAsync(async () =>
+            {
+                var request = CheckSessionRequest(token);
+                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
+                return _responseBuilder.GetResponseData<List<object>>(response);
+            }).ConfigureAwait(false);
+        }
+
+        public async Task<Contracts.Result.BaseResult<string>> GetAllAsync(string token)
+        {
+            return await Proxy().ExecuteAsync(async () =>
+            {
+                var request = GetAllRequest(token);
+                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
+                return _responseBuilder.GetResponseData<List<object>>(response);
+            }).ConfigureAwait(false);
+        }
+
+        public async Task<Contracts.Result.BaseResult<string>> GetRolesByUsernameAsync(string token, string userName)
+        {
+            return await Proxy().ExecuteAsync(async () =>
+            {
+                var request = GetRolesByUsernameRequest(token, userName);
+                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
+                return _responseBuilder.GetResponseData<List<object>>(response);
+            }).ConfigureAwait(false);
+        }
+
+        public async Task<Contracts.Result.BaseResult<string>> GetUserMenus(string token)
+        {
+            return await Proxy().ExecuteAsync(async () =>
+            {
+                var request = GetUserMenusRequest(token);
+                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
+                return _responseBuilder.GetResponseData<List<object>>(response);
+            }).ConfigureAwait(false);
+        }
+
+        public async Task<Contracts.Result.BaseResult<string>> GetUserPermissions(string token)
+        {
+            return await Proxy().ExecuteAsync(async () =>
+            {
+                var request = GetUserPermissionsRequest(token);
+                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
+                return _responseBuilder.GetResponseData<List<object>>(response);
+            }).ConfigureAwait(false);
+        }
+
+        public async Task<Contracts.Result.BaseResult<string>> Logout()
+        {
+            return await Proxy().ExecuteAsync(async () =>
+            {
+                var request = GetLogoutRequest();
+                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
+                return _responseBuilder.GetResponseData<List<object>>(response);
+            }).ConfigureAwait(false);
+        }
+
+        public async Task<BaseResult<string>> SignInAsync(Sfc.Wms.App.Api.Contracts.Entities.LoginCredentials loginCredentials)
         {
             return await Proxy().ExecuteAsync(async () =>
             {
@@ -38,27 +108,76 @@ namespace Sfc.Wms.App.Api.Nuget.Gateways
             }).ConfigureAwait(false);
         }
 
-        public async Task<BaseResult<string>> GetAllAsync(string token)
+        public async Task<Contracts.Result.BaseResult<string>> UpdateUserRoles(UserRoleModel userRoleModel, string token)
         {
             return await Proxy().ExecuteAsync(async () =>
             {
-                var request = GetAllRequest(token);
+                var request = UpdateUserRolesRequest(userRoleModel, token);
                 var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
                 return _responseBuilder.GetResponseData<List<object>>(response);
             }).ConfigureAwait(false);
         }
 
-        public async Task<BaseResult<string>> GetRolesByUsernameAsync(string token, string userName)
+        public async Task<Contracts.Result.BaseResult<string>> UserPreferences(UserPreferencesModel userPreferencesModel, string token)
         {
             return await Proxy().ExecuteAsync(async () =>
             {
-                var request = GetRolesByUsernameRequest(token, userName);
+                var request = UpdateUserPreferencesRequest(userPreferencesModel, token);
                 var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
                 return _responseBuilder.GetResponseData<List<object>>(response);
             }).ConfigureAwait(false);
         }
 
-        private RestRequest SignInRequest(LoginCredentials loginCredentials , string RestUrl)
+        private string BuildUriString(string path, string RestUrl)
+        {
+            var UrlBuilder = new UriBuilder(RestUrl);
+            UrlBuilder.Path += $"{_endPoint}/{path}";
+            return UrlBuilder.Uri.ToString();
+        }
+
+        private RestRequest CheckSessionRequest(string token)
+        {
+            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.CheckSession}";
+            return GetRequest(token, resource);
+        }
+
+        private RestRequest GetAllRequest(string token)
+        {
+            var resource = $"{_endPoint}/{Routes.Paths.AllUsers}";
+            return GetRequest(token, resource);
+        }
+
+        private RestRequest GetLogoutRequest()
+        {
+            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.Logout}";
+            return GetRequest(null, resource);
+        }
+
+        private RestRequest GetRolesByUsernameRequest(string token, string userName)
+        {
+            var resource = $"{_endPoint}/{Routes.Paths.UserRoles}/{userName}";
+            return GetRequest(token, resource);
+        }
+
+        private RestRequest GetUserMenusRequest(string token)
+        {
+            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.Menu}";
+            return GetRequest(token, resource);
+        }
+
+        private RestRequest GetUserPermissionsRequest(string token)
+        {
+            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.UserRolePermissions}";
+            return GetRequest(token, resource);
+        }
+
+        private RestRequest PostChangePasswordRequest(ChangePasswordModel changePasswordModel, string token)
+        {
+            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.ChangePassword}";
+            return PostRequest(token, changePasswordModel, resource);
+        }
+
+        private RestRequest SignInRequest(Sfc.Wms.App.Api.Contracts.Entities.LoginCredentials loginCredentials, string RestUrl)
         {
             var resource = BuildUriString(Routes.Paths.UserLogin, RestUrl);
             var body = new
@@ -69,134 +188,16 @@ namespace Sfc.Wms.App.Api.Nuget.Gateways
             return PostRequest(resource, body, null);
         }
 
-        private string BuildUriString(string path,string RestUrl)
+        private RestRequest UpdateUserPreferencesRequest(UserPreferencesModel userPreferencesModel, string token)
         {
-			var UrlBuilder = new UriBuilder(RestUrl);
-            UrlBuilder.Path += $"{_endPoint}/{path}";
-            return UrlBuilder.Uri.ToString();
-        }
-
-        private RestRequest GetAllRequest(string token)
-        {
-            var resource = $"{_endPoint}/{Routes.Paths.AllUsers}";
-            return GetRequest(token, resource);
-        }
-
-        private RestRequest GetRolesByUsernameRequest(string token, string userName)
-        {
-            var resource = $"{_endPoint}/{Routes.Paths.UserRoles}/{userName}";
-            return GetRequest(token, resource);
-        }
-
-        public async Task<BaseResult<string>> CheckSession(string token)
-        {
-            return await Proxy().ExecuteAsync(async () =>
-            {
-                var request = CheckSessionRequest(token);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
-            }).ConfigureAwait(false);
-        }
-
-        private RestRequest CheckSessionRequest(string token)
-        {
-            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.CheckSession}";
-            return GetRequest(token, resource);
-        }
-
-        public async Task<BaseResult<string>> GetUserPermissions(string token)
-        {
-            return await Proxy().ExecuteAsync(async () =>
-            {
-                var request = GetUserPermissionsRequest(token);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
-            }).ConfigureAwait(false);
-        }
-        private RestRequest GetUserPermissionsRequest(string token)
-        {
-            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.UserRolePermissions}";
-            return GetRequest(token, resource);
-        }
-
-        public async Task<BaseResult<string>> GetUserMenus(string token)
-        {
-            return await Proxy().ExecuteAsync(async () =>
-            {
-                var request = GetUserMenusRequest(token);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
-            }).ConfigureAwait(false);
-        }
-
-        private RestRequest GetUserMenusRequest(string token)
-        {
-            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.Menu}";
-            return GetRequest(token, resource);
-        }
-
-        public async Task<BaseResult<string>> Logout()
-        {
-            return await Proxy().ExecuteAsync(async () =>
-            {
-                var request = GetLogoutRequest();
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
-            }).ConfigureAwait(false);
-        }
-
-        private RestRequest GetLogoutRequest()
-        {
-            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.Logout}";
-            return GetRequest(null, resource);
-        }
-
-        public async Task<BaseResult<string>> ChangePassword(ChangePasswordModel changePasswordModel, string token)
-        {
-            return await Proxy().ExecuteAsync(async () =>
-            {
-                var request = PostChangePasswordRequest(changePasswordModel,token);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
-            }).ConfigureAwait(false);
-        }
-
-        private RestRequest PostChangePasswordRequest(ChangePasswordModel changePasswordModel, string token)
-        {
-            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.ChangePassword}";
-            return PostRequest(token, changePasswordModel, resource);
-        }
-
-        public async Task<BaseResult<string>> UpdateUserRoles(UserRoleModel userRoleModel, string token)
-        {
-            return await Proxy().ExecuteAsync(async () =>
-            {
-                var request = UpdateUserRolesRequest(userRoleModel, token);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
-            }).ConfigureAwait(false);
+            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.UserPreferences}";
+            return PutRequest(resource, userPreferencesModel, token);
         }
 
         private RestRequest UpdateUserRolesRequest(UserRoleModel userRoleModel, string token)
         {
             var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.UserRoles}";
             return PostRequest(token, userRoleModel, resource);
-        }
-
-        public async Task<BaseResult<string>> UserPreferences(UserPreferencesModel userPreferencesModel, string token)
-        {
-            return await Proxy().ExecuteAsync(async () =>
-            {
-                var request = UpdateUserPreferencesRequest(userPreferencesModel, token);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
-            }).ConfigureAwait(false);
-        }
-
-        private RestRequest UpdateUserPreferencesRequest(UserPreferencesModel userPreferencesModel, string token)
-        {
-            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Prefixes.UserPreferences}";
-            return PutRequest(resource, userPreferencesModel, token);
         }
     }
 }
