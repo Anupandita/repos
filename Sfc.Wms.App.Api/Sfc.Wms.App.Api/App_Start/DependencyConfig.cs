78e3d47724171447f66c4127cbbab8fc58bf6b5a
Radha Pidathala <radha.pidathala@logixpace.net>
2019-10-28 17:08:20 +0530

    Removed explicit registering of type

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api/App_Start/DependencyConfig.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/App_Start/DependencyConfig.cs
@@ -4,8 +4,6 @@ using Sfc.Core.Cache.Contracts;
 using Sfc.Core.Cache.InMemory;
 using Sfc.Core.OnPrem.Security.Contracts.Extensions;
 using Sfc.Wms.App.App.AutoMapper;
-using Sfc.Wms.Configuration.Security.Rbac.Repository.Context;
-using Sfc.Wms.Configuration.Security.Rbac.Repository.Interfaces;
 using Sfc.Wms.Data.Context;
 using Sfc.Wms.Foundation.InboundLpn.Repository.LocationDataRepository;
 using Sfc.Wms.Framework.Security.Rbac.AutoMapper;
@@ -62,7 +60,6 @@ namespace Sfc.Wms.App.Api
             container.Register(() => ConfigurationManager.AppSettings["db:encryptionKey"].ToSecureString(), Lifestyle.Singleton);
             container.Register<ISfcCache>(() => new SfcInMemoryCache(MemoryCache.Default), Lifestyle.Scoped);
             container.Register<IMappingFixture>(() => new MappingFixture(), Lifestyle.Singleton);
-            container.Register<IUserRepository, UserRepository>(Lifestyle.Scoped);
             container.Register<LocationData>(Lifestyle.Scoped);
             container.Register<LpnParameterValidator>(Lifestyle.Scoped);
 
