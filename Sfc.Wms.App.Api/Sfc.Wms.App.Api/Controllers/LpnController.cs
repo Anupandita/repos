b3ba56d3bac9f47a9b0c25354e479b7003aac59f
Bharani Thungapalli <bharani.thungapalli@wmapparatus.com>
2019-11-17 01:56:41 -0700

    Merged PR 1396: Merge 141918_LpnLockUnLock to integration
    Added new endpoint for LpnLockUnlock.
    Reverted DateTime changes.
    UserId update for UserMaster Table.
    
    Related work items: #141918

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/LpnController.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/LpnController.cs
@@ -1,16 +1,15 @@
-﻿using Sfc.Core.BaseApiController;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Sfc.Core.BaseApiController;
 using Sfc.Core.OnPrem.Result;
 using Sfc.Wms.App.Api.Contracts.Constants;
 using Sfc.Wms.Foundation.InboundLpn.Contracts.Dtos;
 using Sfc.Wms.Foundation.InboundLpn.Contracts.Interfaces;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Description;
 
 namespace Sfc.Wms.App.Api.Controllers
 {
-
     [Authorize]
     [RoutePrefix(Routes.Prefixes.Lpn)]
     public class LpnController : SfcBaseController
@@ -22,30 +21,12 @@ namespace Sfc.Wms.App.Api.Controllers
             _lpnService = lpnService;
         }
 
-        [HttpGet]
-        [Route(Routes.Paths.Find)]
-        [ResponseType(typeof(BaseResult<FindLpnDto>))]
-        public async Task<IHttpActionResult> FindLpnAsync([FromUri]LpnParamModel lpnParamModel)
-        {
-            var response = await _lpnService.FindLpnAsync(lpnParamModel).ConfigureAwait(false);
-            return ResponseHandler(response);
-        }
-
-        [HttpGet]
-        [Route(Routes.Paths.LpnHistory)]
-        [ResponseType(typeof(BaseResult<List<LpnHistoryDto>>))]
-        public async Task<IHttpActionResult> GetLpnHistoryAsync(string lpnNumber,string warehouse )
-        {
-            var response = await _lpnService.GetLpnHistoryAsync(warehouse, lpnNumber).ConfigureAwait(false);
-            return ResponseHandler(response);
-        }
-
         [HttpPost]
-        [Route(Routes.Paths.LpnAisleTrans)]
-        [ResponseType(typeof(BaseResult<AisleTransactionDto>))]
-        public async Task<IHttpActionResult> GetAisleTransactionAsync(string lpnId, string faceLocationId)
+        [Route(Routes.Paths.LpnCommentsAdd)]
+        [ResponseType(typeof(BaseResult))]
+        public async Task<IHttpActionResult> AddLpnCommentAsync(CaseCommentDto caseComment)
         {
-            var response = await _lpnService.GetAisleTransactionAsync(lpnId, faceLocationId).ConfigureAwait(false);
+            var response = await _lpnService.AddLpnCommentAsync(caseComment).ConfigureAwait(false);
             return ResponseHandler(response);
         }
 
@@ -59,11 +40,56 @@ namespace Sfc.Wms.App.Api.Controllers
         }
 
         [HttpGet]
+        [Route(Routes.Paths.Find)]
+        [ResponseType(typeof(BaseResult<FindLpnDto>))]
+        public async Task<IHttpActionResult> FindLpnAsync([FromUri]LpnParamModel lpnParamModel)
+        {
+            var response = await _lpnService.FindLpnAsync(lpnParamModel).ConfigureAwait(false);
+            return ResponseHandler(response);
+        }
+
+        [HttpPost]
+        [Route(Routes.Paths.LpnAisleTrans)]
+        [ResponseType(typeof(BaseResult<AisleTransactionDto>))]
+        public async Task<IHttpActionResult> GetAisleTransactionAsync(string lpnId, string faceLocationId)
+        {
+            var response = await _lpnService.GetAisleTransactionAsync(lpnId, faceLocationId).ConfigureAwait(false);
+            return ResponseHandler(response);
+        }
+
+        [HttpGet]
         [Route(Routes.Paths.LpnComments)]
         [ResponseType(typeof(BaseResult<List<CaseCommentDto>>))]
         public async Task<IHttpActionResult> GetLpnCommentsAsync(string lpnId)
         {
             var response = await _lpnService.GetLpnCommentsAsync(lpnId).ConfigureAwait(false);
+            return ResponseHandler(response);
+        }
+
+        [HttpGet]
+        [Route(Routes.Paths.LpnDetails)]
+        [ResponseType(typeof(BaseResult<List<CaseDetailDto>>))]
+        public async Task<IHttpActionResult> GetLpnDetailsAsync(string lpnId)
+        {
+            var response = await _lpnService.GetLpnDetailsAsync(lpnId).ConfigureAwait(false);
+            return ResponseHandler(response);
+        }
+
+        [HttpGet]
+        [Route(Routes.Paths.LpnHistory)]
+        [ResponseType(typeof(BaseResult<List<LpnHistoryDto>>))]
+        public async Task<IHttpActionResult> GetLpnHistoryAsync(string lpnNumber, string warehouse)
+        {
+            var response = await _lpnService.GetLpnHistoryAsync(warehouse, lpnNumber).ConfigureAwait(false);
+            return ResponseHandler(response);
+        }
+
+        [HttpGet]
+        [Route(Routes.Paths.LpnLockUnlock)]
+        [ResponseType(typeof(BaseResult<List<CaseDetailDto>>))]
+        public async Task<IHttpActionResult> GetLpnLockUnlockAsync(string lpnId)
+        {
+            var response = await _lpnService.GetLpnLockUnlockAsync(lpnId).ConfigureAwait(false);
             return ResponseHandler(response);
         }
 
@@ -82,24 +108,6 @@ namespace Sfc.Wms.App.Api.Controllers
         public async Task<IHttpActionResult> UpdateLpnCaseDetailsAsync(LpnCaseDetailsUpdateDto lpnCaseDetailsUpdate)
         {
             var response = await _lpnService.UpdateLpnCaseDetails(lpnCaseDetailsUpdate).ConfigureAwait(false);
-            return ResponseHandler(response);
-        }
-
-        [HttpPost]
-        [Route(Routes.Paths.LpnCommentsAdd)]
-        [ResponseType(typeof(BaseResult))]
-        public async Task<IHttpActionResult> AddLpnCommentAsync(CaseCommentDto caseComment)
-        {
-            var response = await _lpnService.AddLpnCommentAsync(caseComment).ConfigureAwait(false);
-            return ResponseHandler(response);
-        }
-
-        [HttpGet]
-        [Route(Routes.Paths.LpnDetails)]
-        [ResponseType(typeof(BaseResult<List<CaseDetailDto>>))]
-        public async Task<IHttpActionResult> GetLpnDetailsAsync(string lpnId)
-        {
-            var response = await _lpnService.GetLpnDetailsAsync(lpnId).ConfigureAwait(false);
             return ResponseHandler(response);
         }
     }
