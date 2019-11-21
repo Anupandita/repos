690b9f0a357290b1a264076d1c010573fa285870
Raju Samala <raju_samala@shamrockfoods.com>
2019-11-21 07:15:48 -0700

    Merged PR 1426: Added SystemCode Dto
    Added SystemCode Dto
    
    Related work items: #134142

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api.Contracts/Sfc.Wms.App.Api.Contracts/Interfaces/ICommonGateway.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Contracts/Sfc.Wms.App.Api.Contracts/Interfaces/ICommonGateway.cs
@@ -5,6 +5,6 @@ namespace Sfc.Wms.App.Api.Contracts.Interfaces
 {
     public interface ICommonGateway
     {
-        Task<BaseResult<string>> CodeIds(string isWhseSysCode, string recType, string codeType, bool isNumber, string orderByColumn, string token);
+        Task<BaseResult<T>> CodeIds<T>(string isWhseSysCode, string recType, string codeType, bool isNumber, string orderByColumn, string token);
     }
 }
