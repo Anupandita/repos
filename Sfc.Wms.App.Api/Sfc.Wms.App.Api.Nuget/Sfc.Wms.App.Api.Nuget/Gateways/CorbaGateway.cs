73d3c0d62169c5c71a208000a09fcfd513556fe5
Patha Alok <Patha_Alok@shamrockfoods.com>
2019-11-24 08:38:55 -0700

    Merged PR 1456: Corba Api Nuget Changes.
    Corba Api Nuget Changes.
    
    Related work items: #142656

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Gateways/CorbaGateway.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Gateways/CorbaGateway.cs
@@ -1,11 +1,10 @@
 ﻿using RestSharp;
+using Sfc.Core.OnPrem.Result;
+using Sfc.Core.RestResponse;
 using Sfc.Wms.App.Api.Contracts.Constants;
 using Sfc.Wms.App.Api.Contracts.Entities;
 using Sfc.Wms.App.Api.Contracts.Interfaces;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using Sfc.Core.OnPrem.Result;
-using Sfc.Core.RestResponse;
 
 namespace Sfc.Wms.App.Api.Nuget.Gateways
 {
@@ -14,45 +13,49 @@ namespace Sfc.Wms.App.Api.Nuget.Gateways
         private readonly string _endPoint;
         private readonly IResponseBuilder _responseBuilder;
         private readonly IRestClient _restClient;
+        private readonly IRestClient _restCsharpClient;
 
         public CorbaGateway(IResponseBuilder responseBuilders, IRestClient restClient)
         {
             _endPoint = Routes.Prefixes.Corba;
             _responseBuilder = responseBuilders;
             _restClient = restClient;
+            _restCsharpClient =
+                new RestClient(ServiceUrl); //TODO: This variable will be removed after all endpoints were moved to C#.
         }
 
-
-        public async Task<BaseResult<string>> ProcessSingleCorbaCall(string functionName, string className, string isVector, string token, params CorbaModel[] corbaModel)
+        public async Task<BaseResult<T>> ProcessBatchCorbaCall<T>(string functionName, string className, string isVector, string token, params CorbaModel[] corbaModel)
         {
-            return await Proxy().ExecuteAsync(async () =>
+            var retryPolicy = Proxy();
+            return await retryPolicy.ExecuteAsync(async () =>
             {
-                var request = GetSingleCorbaCallRequest(functionName, className,isVector , token,corbaModel);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
+                var request = GetBatchCorbaCallRequest(functionName, className, isVector, token, corbaModel);
+                var response = await _restCsharpClient.ExecuteTaskAsync<T>(request).ConfigureAwait(false);
+                return _responseBuilder.GetBaseResult<T>(response);
             }).ConfigureAwait(false);
         }
 
-        private RestRequest GetSingleCorbaCallRequest(string functionName, string className, string isVector, string token, CorbaModel[] corbaModel)
+        public async Task<BaseResult<T>> ProcessSingleCorbaCall<T>(string functionName, string className, string isVector, string token, params CorbaModel[] corbaModel)
         {
-            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Paths.Single}{Routes.Paths.QueryParamSeperator}{className}{Routes.Paths.QueryParamSeperator}{functionName}{Routes.Paths.QueryParamSeperator}{isVector}";
-            return PostRequest(resource, corbaModel[0], token);
-        }
-
-        public async Task<BaseResult<string>> ProcessBatchCorbaCall(string functionName, string className, string isVector, string token, params CorbaModel[] corbaModel)
-        {
-            return await Proxy().ExecuteAsync(async () =>
+            var retryPolicy = Proxy();
+            return await retryPolicy.ExecuteAsync(async () =>
             {
-                var request = GetBatchCorbaCallRequest(functionName, className, isVector, token, corbaModel);
-                var response = await _restClient.ExecuteTaskAsync<List<object>>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<List<object>>(response);
+                var request = GetSingleCorbaCallRequest(functionName, className, isVector, token, corbaModel);
+                var response = await _restCsharpClient.ExecuteTaskAsync<T>(request).ConfigureAwait(false);
+                return _responseBuilder.GetBaseResult<T>(response);
             }).ConfigureAwait(false);
         }
 
         private RestRequest GetBatchCorbaCallRequest(string functionName, string className, string isVector, string token, CorbaModel[] corbaModel)
         {
             var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Paths.Batch}{Routes.Paths.QueryParamSeperator}{className}{Routes.Paths.QueryParamSeperator}{functionName}{Routes.Paths.QueryParamSeperator}{isVector}";
-            return PostRequest(resource, corbaModel, token);
+            return PostRequest(resource, corbaModel, token, Constants.Authorization);
+        }
+
+        private RestRequest GetSingleCorbaCallRequest(string functionName, string className, string isVector, string token, CorbaModel[] corbaModel)
+        {
+            var resource = $"{_endPoint}{Routes.Paths.QueryParamSeperator}{Routes.Paths.Single}{Routes.Paths.QueryParamSeperator}{className}{Routes.Paths.QueryParamSeperator}{functionName}{Routes.Paths.QueryParamSeperator}{isVector}";
+            return PostRequest(resource, corbaModel[0], token, Constants.Authorization);
         }
     }
 }
