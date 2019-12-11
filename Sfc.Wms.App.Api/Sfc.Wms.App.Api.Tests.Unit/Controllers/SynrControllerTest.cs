24734292c4b790bfbb37628b1452c85eea9c342e
Kumara A R <kumara.ar@sigsenz.com>
2019-12-11 12:54:53 +0530

    Synr api

---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Tests.Unit/Controllers/SynrControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfc.Wms.App.Api.Tests.Unit.Fixtures;
+
+namespace Sfc.Wms.App.Api.Tests.Unit.Controllers
+{
+    [TestClass]
+    public class SynrControllerTest : SynrFixture
+    {
+        [TestMethod]
+        [TestCategory("UNIT")]
+        public void Process_Synr_Message_With_Valid_SynchronizationRequest_Message()
+        {
+            ValidSynchronizationRequestMessage();
+            InsertMessageInvoked();
+            SynchronizationRequestMessageShouldBeProcessed();
+        }
+
+        [TestMethod]
+        [TestCategory("UNIT")]
+        public void Process_Synr_Message_With_Invalid_SynchronizationRequest_Message()
+        {
+            InvalidSynchronizationRequestMessage();
+            InsertMessageInvoked();
+            SynchronizationRequestMessageShouldNotBeProcessed();
+        }
+    }
+}
---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Tests.Unit/Fixtures/SynrFixture.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sfc.Core.OnPrem.Result;
+using Sfc.Wms.App.Api.Controllers;
+using Sfc.Wms.Interfaces.Asrs.Contracts.Dtos;
+using Sfc.Wms.Interfaces.Asrs.Contracts.Interfaces;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Sfc.Wms.App.Api.Tests.Unit.Fixtures
+{
+    public abstract class SynrFixture
+    {
+        private readonly SynchronizationRequestController _synchronizationRequestController;
+        private readonly Mock<IWmsToEmsMessageProcessorService> _messageTypeService;
+        private Task<IHttpActionResult> _testResult;
+
+        protected SynrFixture()
+        {
+            _messageTypeService = new Mock<IWmsToEmsMessageProcessorService>(MockBehavior.Default);
+            _synchronizationRequestController = new SynchronizationRequestController(_messageTypeService.Object);
+        }
+
+        protected void ValidSynchronizationRequestMessage()
+        {
+            var response = new BaseResult()
+            {
+                ResultType = ResultTypes.Created
+            };
+
+            _messageTypeService.Setup(el => el.SynchronizationRequestAsync())
+                .Returns(Task.FromResult(response));
+        }
+
+        protected void InvalidSynchronizationRequestMessage()
+        {
+            var response = new BaseResult()
+            {
+                ResultType = ResultTypes.BadRequest
+            };
+
+            _messageTypeService.Setup(el => el.SynchronizationRequestAsync())
+                .Returns(Task.FromResult(response));
+        }
+
+        protected void InsertMessageInvoked()
+        {
+            _testResult = _synchronizationRequestController.CreateAsync();
+        }
+
+        protected void SynchronizationRequestMessageShouldBeProcessed()
+        {
+            var result = _testResult.Result as NegotiatedContentResult<BaseResult>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.Created, result.Content.ResultType);
+        }
+
+        protected void SynchronizationRequestMessageShouldNotBeProcessed()
+        {
+            var result = _testResult.Result as NegotiatedContentResult<BaseResult>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.BadRequest, result.Content.ResultType);
+        }
+    }
+}
