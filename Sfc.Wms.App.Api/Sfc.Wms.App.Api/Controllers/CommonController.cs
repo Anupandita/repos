a0d4404c29949fc91e2615cbc7899c1618e9519e
Radha Pidathala <radha.pidathala@logixpace.net>
2019-12-24 17:10:45 +0530

    DataTypeOptimization Package update

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/CommonController.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/CommonController.cs
@@ -5,9 +5,11 @@ using Sfc.Wms.App.Api.Contracts.Dto;
 using Sfc.Wms.Configuration.SystemCode.Contracts.Dtos;
 using Sfc.Wms.Configuration.SystemCode.Contracts.Interfaces;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using Sfc.Core.OnPrem.Pagination;
 
 namespace Sfc.Wms.App.Api.Controllers
 {
@@ -27,7 +29,15 @@ namespace Sfc.Wms.App.Api.Controllers
         [ResponseType(typeof(BaseResult<IEnumerable<SysCodeDto>>))]
         public async Task<IHttpActionResult> GetSystemCodesAsync([FromUri] SystemCodeDto sysCodeDto)
         {
-            var resultResponse = await _systemCodeService.GetSystemCodeAsync(sysCodeDto.RecType, sysCodeDto.CodeType, sysCodeDto.CodeId, sysCodeDto.OrderByColumn, sysCodeDto.OrderBy);
+            //TODO: Need to update SystemCodeDto
+            var resultResponse = await _systemCodeService.GetSystemCodeAsync(sysCodeDto.RecType, sysCodeDto.CodeType,
+                sysCodeDto.CodeId, 
+                new SortOption
+                {
+                    PropertyName = sysCodeDto.OrderByColumn,
+                    OrderBy = sysCodeDto.OrderBy == "asc" ? ListSortDirection.Ascending : ListSortDirection.Descending
+                });
+              //  sysCodeDto.OrderByColumn, sysCodeDto.OrderBy);
             return ResponseHandler(resultResponse);
         }
     }
