b7c586f8b76c25e355d01d8613e746605c4e65b4
Bharani Thungapalli <ven_apparatus1@shamrockfoods.com>
2020-01-14 03:55:24 -0700

    Merged PR 1805: Merge Feaature/148080_Model_Change to integration
    Related work items: #148080

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Interfaces/IUserMasterGateway.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Interfaces/IUserMasterGateway.cs
@@ -2,12 +2,13 @@
 using Sfc.Wms.Configuration.UserMaster.Contracts.Dtos;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Sfc.Wms.App.Api.Contracts.Entities;
 
 namespace Sfc.Wms.App.Api.Nuget.Interfaces
 {
     public interface IUserMasterGateway
     {
         Task<BaseResult> CreateUserPreferences(SwmUserSettingDto swmUserSettingDto, string token);
-        Task<BaseResult> UpdateUserPreferences(SwmUserSettingDto swmUserSettingDto, string token);
+        Task<BaseResult> UpdateUserPreferences(UserPreferencesModel userPreferencesModel, string token);
     }
 }
