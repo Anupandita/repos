a973314080fa10250370ef11790e5a54d9ef9ea5
Radha Pidathala <radha.pidathala@logixpace.net>
2020-01-29 16:14:18 +0530

    Added corbacontroller unit test cases

---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Tests.Unit/Controllers/CorbaControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfc.Wms.App.Api.Tests.Unit.Constants;
+using Sfc.Wms.App.Api.Tests.Unit.Fixtures;
+
+namespace Sfc.Wms.App.Api.Tests.Unit.Controllers
+{
+    [TestClass]
+    [TestCategory(TestCategories.Unit)]
+    public class CorbaControllerTest : CorbaControllerFixture
+    {
+        [TestMethod]
+        public void Single_Corba_Invocation_Returned_Ok_As_Response_Status()
+        {
+            ValidInputParametersForSingleCorba();
+            SingleCorbaInvoked();
+            SingleCorbaInvocationReturnedOkAsResponse();
+        }
+
+        [TestMethod]
+        public void Single_Corba_Invocation_Returned_Exception_As_Response_Status()
+        {
+            InValidInputParametersForSingleCorba();
+            SingleCorbaInvoked();
+            SingleCorbaInvocationReturnedExceptionAsResponse();
+        }
+
+
+        [TestMethod]
+        public void Batch_Corba_Invocation_Returned_Ok_As_Response_Status()
+        {
+            ValidInputParametersForBatchCorba();
+            BatchCorbaInvoked();
+            BatchCorbaInvocationReturnedOkAsResponse();
+        }
+
+        [TestMethod]
+        public void Batch_Corba_Invocation_Returned_Exception_As_Response_Status()
+        {
+            InValidInputParametersForBatchCorba();
+            BatchCorbaInvoked();
+            BatchCorbaInvocationReturnedExceptionAsResponse();
+        }
+    }
+}
