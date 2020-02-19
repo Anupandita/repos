8b77a963e0ac53fe782df5776882662b8afab8ac
Radha Pidathala <radha.pidathala@logixpace.net>
2020-02-19 11:44:29 +0530

    Item Attribute end points

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Interfaces/IItemAttributeGateway.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Interfaces/IItemAttributeGateway.cs
@@ -1,14 +1,14 @@
-﻿using System.Threading.Tasks;
-using Sfc.Core.OnPrem.Result;
-using Sfc.Wms.App.Api.Contracts.Entities;
+﻿using Sfc.Core.OnPrem.Result;
+using Sfc.Wms.Configuration.ItemMasters.Contracts.Dtos;
+using System.Threading.Tasks;
 
 namespace Sfc.Wms.App.Api.Nuget.Interfaces
 {
     public interface IItemAttributeGateway
     {
-        Task<BaseResult<string>> GetItemAttributeDetailsAsync(ItemAttributeParamModel itemAttributeParamModel,
+        Task<BaseResult<ItemAttributeSearchResultDto>> SearchAsync(ItemAttributeSearchInputDto itemAttributeSearchInputDto,
             string token);
 
-        Task<BaseResult<string>> GetDrillDownItemAttributeAsync(string item, string token);
+        Task<BaseResult<ItemAttributeDetailsDto>> AttributeDrillDownAsync(string item, string token);
     }
 }
