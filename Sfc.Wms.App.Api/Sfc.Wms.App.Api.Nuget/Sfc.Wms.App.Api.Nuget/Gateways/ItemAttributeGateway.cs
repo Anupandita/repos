6251923c7f61314592d0b7875d9c27a8063b70cf
Steven Saurini <Steven_Saurini@shamrockfoods.com>
2020-02-28 13:07:12 -0700

    Merged PR 2048: 1.1.1.51
    Related work items: #152332, #152463, #152778, #154284, #154820, #154911, #155204

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Gateways/ItemAttributeGateway.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Nuget/Sfc.Wms.App.Api.Nuget/Gateways/ItemAttributeGateway.cs
@@ -1,11 +1,10 @@
-﻿using System.Threading.Tasks;
-using RestSharp;
-using Sfc.Core.OnPrem.Result;
+﻿using Sfc.Core.OnPrem.Result;
 using Sfc.Core.RestResponse;
 using Sfc.Wms.App.Api.Contracts.Constants;
-using Sfc.Wms.App.Api.Contracts.Entities;
-using Sfc.Wms.App.Api.Nuget.Builders;
 using Sfc.Wms.App.Api.Nuget.Interfaces;
+using Sfc.Wms.Configuration.ItemMasters.Contracts.Dtos;
+using System;
+using System.Threading.Tasks;
 
 namespace Sfc.Wms.App.Api.Nuget.Gateways
 {
@@ -13,61 +12,47 @@ namespace Sfc.Wms.App.Api.Nuget.Gateways
     {
         private readonly string _endPoint;
         private readonly IResponseBuilder _responseBuilder;
-        private readonly IRestClient _restClient;
+        private readonly IRestCsharpClient _restCsharpClient;
+        private const string Authorization = "Authorization";
 
-        public ItemAttributeGateway(IResponseBuilder responseBuilders, IRestClient restClient) : base(restClient)
+
+        public ItemAttributeGateway(IResponseBuilder responseBuilders, IRestCsharpClient restClient) : base(restClient)
         {
             _endPoint = Routes.Prefixes.ItemAttribute;
             _responseBuilder = responseBuilders;
-            _restClient = restClient;
+            restClient.BaseUrl = new Uri(ServiceUrl);
+            _restCsharpClient = restClient;
         }
 
-        public async Task<BaseResult<string>> GetItemAttributeDetailsAsync(
-            ItemAttributeParamModel itemAttributeParamModel, string token)
+        public async Task<BaseResult<ItemAttributeSearchResultDto>> SearchAsync(
+            ItemAttributeSearchInputDto itemAttributeSearchInputDto, string token)
         {
             var retryPolicy = Proxy();
             return await retryPolicy.ExecuteAsync(async () =>
             {
-                var request = GetItemAttributeDetailsRequest(itemAttributeParamModel, token);
-                var response = await _restClient.ExecuteTaskAsync<object>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<string>(response);
+                var resource = $"{_endPoint}/{Routes.Paths.Search}";
+                var request = GetRequest(resource, itemAttributeSearchInputDto, token, Authorization);
+
+                var response = await _restCsharpClient.ExecuteTaskAsync<BaseResult<ItemAttributeSearchResultDto>>(request)
+                    .ConfigureAwait(false);
+
+                return _responseBuilder.GetBaseResult<ItemAttributeSearchResultDto>(response);
             }).ConfigureAwait(false);
         }
 
-        public async Task<BaseResult<string>> GetDrillDownItemAttributeAsync(string item, string token)
+        public async Task<BaseResult<ItemAttributeDetailsDto>> AttributeDrillDownAsync(string item, string token)
         {
             var retryPolicy = Proxy();
             return await retryPolicy.ExecuteAsync(async () =>
             {
-                var request = GetDrillDownItemAttributeRequest(item, token);
-                var response = await _restClient.ExecuteTaskAsync<object>(request).ConfigureAwait(false);
-                return _responseBuilder.GetResponseData<string>(response);
+                var resource = $"{_endPoint}/{item}";
+                var request = GetRequest(token, resource, Authorization);
+
+                var response = await _restCsharpClient.ExecuteTaskAsync<BaseResult<ItemAttributeDetailsDto>>(request)
+                    .ConfigureAwait(false);
+
+                return _responseBuilder.GetBaseResult<ItemAttributeDetailsDto>(response);
             }).ConfigureAwait(false);
-        }
-
-        private RestRequest GetItemAttributeDetailsRequest(ItemAttributeParamModel itemAttributeParamModel,
-            string token)
-        {
-            var resource =
-                $"{_endPoint}/{Routes.Paths.SearchItemAttribute}{Routes.Paths.QueryParamSymbol}pageNo={itemAttributeParamModel.pageNo}{Routes.Paths.QueryParamAnd}rowsPerPage={itemAttributeParamModel.rowsPerPage}{Routes.Paths.QueryParamAnd}totalRows={itemAttributeParamModel.totalRows}";
-
-            resource = QueryStringBuilder.BuildQuery("inpt_itemid=", itemAttributeParamModel.inpt_itemid, resource,
-                false);
-            resource = QueryStringBuilder.BuildQuery("inpt_itemdesc=", itemAttributeParamModel.inpt_itemdesc, resource,
-                false);
-            resource = QueryStringBuilder.BuildQuery("inpt_vendoritemnbr=", itemAttributeParamModel.inpt_vendoritemnbr,
-                resource, false);
-            resource = QueryStringBuilder.BuildQuery("inpt_tempzone=", itemAttributeParamModel.inpt_tempzone, resource,
-                false);
-
-            return GetRequest(token, resource);
-        }
-
-        private RestRequest GetDrillDownItemAttributeRequest(string item, string token)
-        {
-            var resource =
-                $"{_endPoint}/{Routes.Paths.DrillDownItemAttribute}{Routes.Paths.QueryParamSymbol}item={item}";
-            return GetRequest(token, resource);
         }
     }
 }
