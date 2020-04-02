0bdbc9a9d592c3e254a755145bbc22cb91aebf8e
Kumara A R <kumara_ar@shamrockfoods.com>
2020-04-02 11:52:39 +0530

    Skmt wrapper using background thread

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/SkuMaintenanceController.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/SkuMaintenanceController.cs
@@ -2,14 +2,14 @@
 using Sfc.Core.OnPrem.Result;
 using Sfc.Wms.App.Api.Contracts.Constants;
 using Sfc.Wms.Interfaces.Asrs.Contracts.Interfaces;
+using SimpleInjector.Lifestyles;
 using System;
-using System.Configuration;
+using System.Collections.Generic;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
-using SimpleInjector.Lifestyles;
 
 namespace Sfc.Wms.App.Api.Controllers
 {
@@ -41,25 +41,32 @@ namespace Sfc.Wms.App.Api.Controllers
         [Route(Routes.Paths.SkmtWrapper)]
         [ResponseType(typeof(BaseResult))]
         [AllowAnonymous]
-        public async Task<IHttpActionResult> CreateAsync(string actionCode)
+        public IHttpActionResult CreateAsync(string actionCode)
         {
-            int.TryParse(ConfigurationManager.AppSettings["SkmtWrapperTimeout"], out var skmtWrapperTimeout);
-            int.TryParse(ConfigurationManager.AppSettings["DefaultTimeout"], out var defaultTimeout);
-            HttpContext.Current.Server.ScriptTimeout = skmtWrapperTimeout;
+            var start = new ParameterizedThreadStart(SKmtWrapper);
+            var skmtThread = new Thread(start) {IsBackground = true};
+            skmtThread.Start(actionCode);
 
+            var result=new BaseResult()
+            {   ResultType = ResultTypes.Ok,
+                ValidationMessages = new List<ValidationMessage>()
+                {
+                    new ValidationMessage(nameof(SKmtWrapper),"Process Started")
+                }
+
+            };
+
+            return Content(HttpStatusCode.OK, result);
+        }
+
+        private static void SKmtWrapper(object actionCode)
+        {
             var container = DependencyConfig.Register();
             using (var scope = AsyncScopedLifestyle.BeginScope(container))
             {
                 var wmsToEmsParallelProcessService = scope.GetInstance<IWmsToEmsParallelProcessService>();
-                var result = await wmsToEmsParallelProcessService
-                    .GetSkmtParallelAsync(container, actionCode)
-                    .ConfigureAwait(false);
-
-                HttpContext.Current.Server.ScriptTimeout = defaultTimeout;
-
-                return Content(Enum.TryParse(result.ResultType.ToString(), out HttpStatusCode statusCode)
-                    ? statusCode
-                    : HttpStatusCode.ExpectationFailed, result);
+                var result = wmsToEmsParallelProcessService
+                    .GetSkmtParallelAsync(container, (string)actionCode).Result;
             }
         }
     }
