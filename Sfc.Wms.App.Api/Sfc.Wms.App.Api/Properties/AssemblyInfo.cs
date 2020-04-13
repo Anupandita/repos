d64df0fbd0605a3f407cb1613a6e4dea5b2e0aa5
ssauri <Steven_Saurini@shamrockfoods.com>
2020-04-13 11:35:41 -0700

    Update to see build version since not using semantic versioning on the rerleases.

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api/Properties/AssemblyInfo.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/Properties/AssemblyInfo.cs
@@ -21,14 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b4d08920-bdaa-4641-b582-18723c7eb7a3")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.6.2")]
-[assembly: AssemblyFileVersion("1.1.6.2")]
+[assembly: AssemblyVersion("1.1.6.4")]
+[assembly: AssemblyFileVersion("1.1.6.4")]
