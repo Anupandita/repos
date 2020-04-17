812335ae76ee1e1e901811e003461f3e46111ba7
Radha Pidathala <radha.pidathala@logixpace.net>
2020-04-17 16:38:10 +0530

    Unit test cases for Receiving controller

---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Tests.Unit/Controllers/ReceivingControllerTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfc.Wms.App.Api.Tests.Unit.Constants;
+using Sfc.Wms.App.Api.Tests.Unit.Fixtures;
+
+namespace Sfc.Wms.App.Api.Tests.Unit.Controllers
+{
+    [TestClass, TestCategory(TestCategories.Unit)]
+    public class ReceivingControllerTest:ReceivingControllerFixture
+    {
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Receipt_Inquiry_Search_Operation_Returned_Ok_As_Response_Status()
+        {
+            InputForReceiptInquiry();
+            SearchOperationInvoked();
+            TheSearchOperationReturnedOkResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Receipt_Inquiry_Search_Operation_Returned_BadRequest_As_Response_Status()
+        {
+            EmptyOrNullInputForReceiptInquiry();
+            SearchOperationInvoked();
+            TheSearchOperationReturnedBadRequestResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Get_Asn_Details_Operation_Returned_Ok_As_Response_Status()
+        {
+            InputToFetchAsnDetails();
+            FetchAsnDetailsOperationInvoked();
+            TheFetchAsnDetailsOperationReturnedOkResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Get_Asn_Details_Operation_Returned_BadRequest_As_Response_Status()
+        {
+            EmptyOrNullInputToFetchAsnDetails();
+            FetchAsnDetailsOperationInvoked();
+            TheFetchAsnDetailsOperationReturnedBadRequestResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Get_Qv_Details_Operation_Returned_Ok_As_Response_Status()
+        {
+            InputToGetQvDetails();
+            GetQvDetailsOperationInvoked();
+            TheGetQvDetailsOperationReturnedOkResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Get_Qv_Details_Operation_Returned_BadRequest_As_Response_Status()
+        {
+            EmptyOrNullInputToGetQvDetails();
+            GetQvDetailsOperationInvoked();
+            TheGetQvDetailsOperationReturnedBadRequestResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Get_Asn_Lot_Track_Details_Operation_Returned_Ok_As_Response_Status()
+        {
+            InputToGetAsnLotTrackDetails();
+            GetAsnLotTrackDetailsOperationInvoked();
+            TheGetAsnLotTrackDetailsOperationReturnedOkResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Get_Asn_Lot_Track_Details_Operation_Returned_BadRequest_As_Response_Status()
+        {
+            EmptyOrNullInputToGetAsnLotTrackDetails();
+            GetAsnLotTrackDetailsOperationInvoked();
+            TheGetAsnLotTrackDetailsOperationReturnedBadRequestResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Get_Asn_Lot_Track_Details_Operation_Returned_NotFound_As_Response_Status()
+        {
+            InputToGetAsnLotTrackDetailsForWhichNoRecordExists();
+            GetAsnLotTrackDetailsOperationInvoked();
+            TheGetAsnLotTrackDetailsOperationReturnedNotFoundAsResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Update_AnswetText_Operation_Returned_Ok_As_Response_Status()
+        {
+            InputToUpdateAnswerText();
+            UpdateAnswerTextOperationInvoked();
+            TheUpdateAnswerTextOperationReturnedOkResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Update_Answer_Text_Operation_Returned_BadRequest_As_Response_Status()
+        {
+            EmptyOrNullInputToUpdateAnswerText();
+            UpdateAnswerTextOperationInvoked();
+            TheUpdateAnswerTextOperationReturnedBadRequestResponse();
+        }
+
+        [TestMethod, TestCategory(TestCategories.Unit)]
+        public void Update_Answer_Text_Operation_Returned_NotFound_As_Response_Status()
+        {
+            InputToUpdateAnswerTextForWhichNoRecordExists();
+            UpdateAnswerTextOperationInvoked();
+            TheUpdateAnswerTextOperationReturnedNotFoundAsResponse();
+        }
+    }
+}
---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api.Tests.Unit/Fixtures/ReceivingControllerFixture.cs
@@ -0,0 +1,330 @@
+﻿using DataGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sfc.Core.OnPrem.Result;
+using Sfc.Wms.App.Api.Controllers;
+using Sfc.Wms.Foundation.Receiving.Contracts.UoW.Dtos;
+using Sfc.Wms.Foundation.Receiving.Contracts.UoW.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Sfc.Wms.App.Api.Tests.Unit.Fixtures
+{
+    public class ReceivingControllerFixture
+    {
+        private readonly ReceivingController _receivingController;
+        private readonly Mock<IInboundReceivingService> _receivingService;
+        private readonly Mock<IQuestionAnswerService> _answerService;
+        private readonly Mock<IAsnLotTrackingService> _lotService;
+        private ReceiptInquiryDto receiptInquiryDto;
+        private AnswerTextDto answerTextDto;
+        private string shipmentNumber;
+        private string skuId;
+        private Task<IHttpActionResult> testResponse;
+
+        protected ReceivingControllerFixture()
+        {
+            _receivingService = new Mock<IInboundReceivingService>(MockBehavior.Default);
+            _answerService = new Mock<IQuestionAnswerService>(MockBehavior.Default);
+            _lotService = new Mock<IAsnLotTrackingService>(MockBehavior.Default);
+            _receivingController =
+                new ReceivingController(_receivingService.Object, _lotService.Object, _answerService.Object);
+        }
+
+
+        private void MockReceiptSearch(ResultTypes resultType)
+        {
+            var result = new BaseResult<SearchResultDto>
+            {
+                ResultType = resultType,
+                Payload = resultType == ResultTypes.Ok ? Generator.Default.Single<SearchResultDto>() : null
+            };
+
+            _receivingService.Setup(el => el.ReceivingSearchAsync(It.IsAny<ReceiptInquiryDto>()))
+                .Returns(Task.FromResult(result));
+        }
+
+        private void VerifyReceiptSearch()
+        {
+            _receivingService.Verify(el => el.ReceivingSearchAsync(It.IsAny<ReceiptInquiryDto>()));
+        }
+
+        protected void InputForReceiptInquiry()
+        {
+            receiptInquiryDto = Generator.Default.Single<ReceiptInquiryDto>();
+            MockReceiptSearch(ResultTypes.Ok);
+        }
+
+        protected void EmptyOrNullInputForReceiptInquiry()
+        {
+            receiptInquiryDto = null;
+            MockReceiptSearch(ResultTypes.BadRequest);
+        }
+
+        protected void SearchOperationInvoked()
+        {
+            testResponse = _receivingController.SearchAsync(receiptInquiryDto);
+        }
+
+        protected void TheSearchOperationReturnedBadRequestResponse()
+        {
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as NegotiatedContentResult<BaseResult<SearchResultDto>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.BadRequest, result.Content.ResultType);
+        }
+
+        protected void TheSearchOperationReturnedOkResponse()
+        {
+            VerifyReceiptSearch();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as OkNegotiatedContentResult<BaseResult<SearchResultDto>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.Ok, result.Content.ResultType);
+        }
+
+        private void MockGetAsnDetails(ResultTypes resultType)
+        {
+            var result = new BaseResult<IEnumerable<AsnDrillDownDetailsDto>>
+            {
+                ResultType = resultType,
+                Payload = resultType == ResultTypes.Ok ? Generator.Default.List<AsnDrillDownDetailsDto>(2) : null
+            };
+
+            _receivingService.Setup(el => el.GetAsnDrillDownDetailsAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(result));
+        }
+
+        private void VerifyGetAsnDetails()
+        {
+            _receivingService.Verify(el => el.GetAsnDrillDownDetailsAsync(It.IsAny<string>()));
+        }
+
+        protected void InputToFetchAsnDetails()
+        {
+            shipmentNumber = Generator.Default.Single<ReceiptInquiryDto>().ShipmentNumber;
+            MockGetAsnDetails(ResultTypes.Ok);
+        }
+
+        protected void EmptyOrNullInputToFetchAsnDetails()
+        {
+            shipmentNumber = null;
+            MockGetAsnDetails(ResultTypes.BadRequest);
+        }
+
+        protected void FetchAsnDetailsOperationInvoked()
+        {
+            testResponse = _receivingController.GetAsnDetailsAsync(shipmentNumber);
+        }
+
+        protected void TheFetchAsnDetailsOperationReturnedBadRequestResponse()
+        {
+            VerifyGetAsnDetails();
+            Assert.IsNotNull(testResponse);
+            var result =
+                testResponse.Result as NegotiatedContentResult<BaseResult<IEnumerable<AsnDrillDownDetailsDto>>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.BadRequest, result.Content.ResultType);
+        }
+
+        protected void TheFetchAsnDetailsOperationReturnedOkResponse()
+        {
+            VerifyGetAsnDetails();
+            Assert.IsNotNull(testResponse);
+            var result =
+                testResponse.Result as OkNegotiatedContentResult<BaseResult<IEnumerable<AsnDrillDownDetailsDto>>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.Ok, result.Content.ResultType);
+        }
+
+        private void MockGetQvDetails(ResultTypes resultType)
+        {
+            var result = new BaseResult<List<QvDetailsDto>>
+            {
+                ResultType = resultType,
+                Payload = resultType == ResultTypes.Ok ? Generator.Default.List<QvDetailsDto>(2).ToList() : null
+            };
+
+            _receivingService.Setup(el => el.GetQvDetailsAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(result));
+        }
+
+        private void VerifyGetQvDetails()
+        {
+            _receivingService.Verify(el => el.GetQvDetailsAsync(It.IsAny<string>()));
+        }
+
+        protected void InputToGetQvDetails()
+        {
+            shipmentNumber = Generator.Default.Single<ReceiptInquiryDto>().ShipmentNumber;
+            MockGetQvDetails(ResultTypes.Ok);
+        }
+
+        protected void EmptyOrNullInputToGetQvDetails()
+        {
+            shipmentNumber = null;
+            MockGetQvDetails(ResultTypes.BadRequest);
+        }
+
+        protected void GetQvDetailsOperationInvoked()
+        {
+            testResponse = _receivingController.GetQvDetails(shipmentNumber);
+        }
+
+        protected void TheGetQvDetailsOperationReturnedBadRequestResponse()
+        {
+            VerifyGetQvDetails();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as NegotiatedContentResult<BaseResult<List<QvDetailsDto>>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.BadRequest, result.Content.ResultType);
+        }
+
+        protected void TheGetQvDetailsOperationReturnedOkResponse()
+        {
+            VerifyGetQvDetails();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as OkNegotiatedContentResult<BaseResult<List<QvDetailsDto>>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.Ok, result.Content.ResultType);
+        }
+
+        private void MockGetAsnLotTrackDetails(ResultTypes resultType)
+        {
+            var result = new BaseResult<IEnumerable<AsnLotTrackingDto>>
+            {
+                ResultType = resultType,
+                Payload = resultType == ResultTypes.Ok ? Generator.Default.List<AsnLotTrackingDto>(2) : null
+            };
+
+            _lotService.Setup(el => el.GetByShipmentAndSkuIdAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(result));
+        }
+
+        private void VerifyGetAsnLotTrackDetails()
+        {
+            _lotService.Verify(el => el.GetByShipmentAndSkuIdAsync(It.IsAny<string>(), It.IsAny<string>()));
+        }
+
+        protected void InputToGetAsnLotTrackDetails()
+        {
+            shipmentNumber = Generator.Default.Single<ReceiptInquiryDto>().ShipmentNumber;
+            skuId = "123980";
+            MockGetAsnLotTrackDetails(ResultTypes.Ok);
+        }
+
+        protected void InputToGetAsnLotTrackDetailsForWhichNoRecordExists()
+        {
+            shipmentNumber = Generator.Default.Single<ReceiptInquiryDto>().ShipmentNumber;
+            skuId = "123980";
+            MockGetAsnLotTrackDetails(ResultTypes.NotFound);
+        }
+
+        protected void EmptyOrNullInputToGetAsnLotTrackDetails()
+        {
+            skuId = shipmentNumber = null;
+            MockGetAsnLotTrackDetails(ResultTypes.BadRequest);
+        }
+
+        protected void GetAsnLotTrackDetailsOperationInvoked()
+        {
+            testResponse = _receivingController.GetAsnLotTrackByShipmentAndSkuIdAsync(shipmentNumber, skuId);
+        }
+
+        protected void TheGetAsnLotTrackDetailsOperationReturnedBadRequestResponse()
+        {
+            VerifyGetAsnLotTrackDetails();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as NegotiatedContentResult<BaseResult<IEnumerable<AsnLotTrackingDto>>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.BadRequest, result.Content.ResultType);
+        }
+
+        protected void TheGetAsnLotTrackDetailsOperationReturnedNotFoundAsResponse()
+        {
+            VerifyGetAsnLotTrackDetails();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as NegotiatedContentResult<BaseResult<IEnumerable<AsnLotTrackingDto>>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.NotFound, result.Content.ResultType);
+        }
+
+        protected void TheGetAsnLotTrackDetailsOperationReturnedOkResponse()
+        {
+            VerifyGetAsnLotTrackDetails();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as OkNegotiatedContentResult<BaseResult<IEnumerable<AsnLotTrackingDto>>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.Ok, result.Content.ResultType);
+        }
+
+       private void MockUpdateAnswerText(ResultTypes resultType)
+        {
+            var result = new BaseResult
+            {
+                ResultType = resultType
+            };
+
+            _answerService.Setup(el => el.UpdateAnswerTextAsync(It.IsAny<AnswerTextDto>()))
+                .Returns(Task.FromResult(result));
+        }
+
+        private void VerifyUpdateAnswerText()
+        {
+            _answerService.Verify(el => el.UpdateAnswerTextAsync(It.IsAny<AnswerTextDto>()));
+        }
+
+        protected void InputToUpdateAnswerText()
+        {
+            answerTextDto = Generator.Default.Single<AnswerTextDto>();
+            MockUpdateAnswerText(ResultTypes.Ok);
+        }
+
+        protected void InputToUpdateAnswerTextForWhichNoRecordExists()
+        {
+            answerTextDto = Generator.Default.Single<AnswerTextDto>();
+            MockUpdateAnswerText(ResultTypes.NotFound);
+        }
+
+        protected void EmptyOrNullInputToUpdateAnswerText()
+        {
+            answerTextDto = null;
+            MockUpdateAnswerText(ResultTypes.BadRequest);
+        }
+
+        protected void UpdateAnswerTextOperationInvoked()
+        {
+            testResponse = _receivingController.UpdateAnswerTextAsync(answerTextDto);
+        }
+
+        protected void TheUpdateAnswerTextOperationReturnedBadRequestResponse()
+        {
+            VerifyUpdateAnswerText();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as NegotiatedContentResult<BaseResult>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.BadRequest, result.Content.ResultType);
+        }
+
+        protected void TheUpdateAnswerTextOperationReturnedNotFoundAsResponse()
+        {
+            VerifyUpdateAnswerText();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as NegotiatedContentResult<BaseResult>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.NotFound, result.Content.ResultType);
+        }
+
+        protected void TheUpdateAnswerTextOperationReturnedOkResponse()
+        {
+            VerifyUpdateAnswerText();
+            Assert.IsNotNull(testResponse);
+            var result = testResponse.Result as OkNegotiatedContentResult<BaseResult>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ResultTypes.Ok, result.Content.ResultType);
+        }
+    }
+}
