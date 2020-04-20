1cd8a9827adac65693769f1db627a8cff9258ca3
Steven Saurini <Steven_Saurini@shamrockfoods.com>
2020-04-20 10:36:21 -0700

    Merged PR 2281: Release 1.1.7.1
    Related work items: #161778

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api/Properties/AssemblyInfo.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b4d08920-bdaa-4641-b582-18723c7eb7a3")]
 
-[assembly: AssemblyVersion("1.1.6.4")]
-[assembly: AssemblyFileVersion("1.1.6.4")]
+[assembly: AssemblyVersion("1.1.7.1")]
+[assembly: AssemblyFileVersion("1.1.7.1")]
