afc779e015bcadb5cf9bb05cdc3c4b9f7edd35ec
Jennifer Robinson <jennifer_robinson@SHAMROCKFOODS.com>
2020-05-07 11:31:53 -0700

    Add Image Controller Update reference for   <package id="Sfc.Wms.App.Api.Contracts" version="5.9.3"

---
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/ImageUrlsController.cs
@@ -0,0 +1,36 @@
+﻿using Sfc.Core.BaseApiController;
+using Sfc.Core.OnPrem.Result;
+using Sfc.Wms.App.Api.Contracts.Constants;
+using Sfc.Wms.Interfaces.Asrs.Contracts.Interfaces;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Sfc.Wms.Foundation.Edm.Contracts.Interfaces;
+using Sfc.Wms.Foundation.Edm.Contracts.Dtos;
+using Sfc.Wms.App.Api.Contracts.Dto;
+
+namespace Sfc.Wms.App.Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix(Routes.Prefixes.ImageUrls)]
+    public class ImageUrlsController : SfcBaseController
+    {
+        private readonly ISfcImageService _sfcImageService;
+        public ImageUrlsController(ISfcImageService sfcImageService)
+        {
+            _sfcImageService = sfcImageService;
+        }
+
+        [HttpGet]
+        [Route(Routes.Paths.ImageUrlsParams)]
+        [ResponseType(typeof(BaseResult<ImageDto>))]
+        public async Task<IHttpActionResult> GetImageAsync([FromUri] ImageUrlDto imageUrlDto)
+        {
+            var response = await _sfcImageService.GetItemImageAsync(imageUrlDto.sku, imageUrlDto?.gtin).ConfigureAwait(false);
+            return ResponseHandler(response);
+        }
+
+    }
+}
