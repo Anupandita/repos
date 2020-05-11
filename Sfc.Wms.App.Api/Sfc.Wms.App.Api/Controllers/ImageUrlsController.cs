2ca834175b10e0b737b800d6af427171a5d0b995
Jennifer Robinson <jennifer_robinson@SHAMROCKFOODS.com>
2020-05-11 04:12:54 -0700

    Version 10

--- Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/ImageUrlsController.cs
+++ Sfc.Wms.App.Api/Sfc.Wms.App.Api/Controllers/ImageUrlsController.cs
@@ -1,19 +1,12 @@
 ﻿using Sfc.Core.BaseApiController;
 using Sfc.Core.OnPrem.Result;
 using Sfc.Wms.App.Api.Contracts.Constants;
-using Sfc.Wms.Interfaces.Asrs.Contracts.Interfaces;
-using System;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
-using Sfc.Wms.Foundation.Edm.Contracts.Interfaces;
-using Sfc.Wms.Foundation.Edm.Contracts.Dtos;
-using Sfc.Wms.App.Api.Contracts.Dto;
-using System.Drawing;
-using System.IO;
-using System.Runtime.InteropServices;
 using System.Net.Http;
+using Sfc.Wms.Foundation.Edm.Contracts.Interfaces;
+using System.IO;
 
 namespace Sfc.Wms.App.Api.Controllers
 {
@@ -24,8 +17,12 @@ namespace Sfc.Wms.App.Api.Controllers
         public ImageUrlsController(ISfcImageService sfcImageService)
         {
             _sfcImageService = sfcImageService;
-        }
 
+            //var context = new MediaDbContext("MediaDbContext");
+            //var _imageRepository = new ImageRepository(context);
+            //_sfcImageService = new SfcImageService(_imageRepository);
+        }
+        
 
         [HttpGet]
         [AllowAnonymous]
@@ -34,7 +31,7 @@ namespace Sfc.Wms.App.Api.Controllers
         public async Task<HttpResponseMessage> Get([FromUri] string sku, string gtin = "")
         {
             HttpResponseMessage  httpResponseMessage = null;
-            var response = await  _sfcImageService.GetItemImageAsync(sku, gtin).ConfigureAwait(false);
+            var response = await  _sfcImageService.GetItemImageAsync(sku, gtin, 0, "T").ConfigureAwait(false);
                      
             if (response.ResultType == ResultTypes.Ok)
                 httpResponseMessage = ByteArrayToImage(response.Payload.ImageBlob);
